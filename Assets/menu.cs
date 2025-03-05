76ea762eccdffa63d5dd12a14b0258c82aee02ea
Romain <romain.deschaseaux@outlook.fr>
2025-03-05 09:24:10 +0100

    menu update

---
+++ Assets/menu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Option()
+    {
+        SceneManager.LoadScene("OptionMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

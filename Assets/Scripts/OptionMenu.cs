3edbef628e2266c64fe26848b517e9092567bf9f
Douxzy <romain.descha2006@gmail.com>
2025-03-06 15:18:08 +0100

    add SFX menu

--- Assets/Scripts/OptionMenu.cs
+++ Assets/Scripts/OptionMenu.cs
@@ -1,21 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class OptionMenu : MonoBehaviour
 {
-    public AudioMixer masterMixer;
-
+    public AudioMixer masterMixer; // Assigner l'AudioMixer dans Unity
 
     public void ReturnToMenu()
     {
         SceneManager.LoadScene("Menu");
     }
 
-    public void SetSound(float soundLevel)
+    public void SetMusicVolume(float volume)
     {
-        masterMixer.SetFloat ("volume", Mathf.Lerp(-80, 20, soundLevel));
+        if (masterMixer != null)
+        {
+            bool exists = masterMixer.GetFloat("MusicVolume", out float currentValue);
+            if (exists)
+            {
+                masterMixer.SetFloat("MusicVolume", Mathf.Lerp(-80, 20, volume));
+            }
+            else
+            {
+                Debug.LogError("❌ MusicVolume n'existe pas dans l'AudioMixer !");
+            }
+        }
+        else
+        {
+            Debug.LogError("❌ AudioMixer non assigné dans l'Inspector !");
+        }
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        if (masterMixer != null)
+        {
+            bool exists = masterMixer.GetFloat("SFXVolume", out float currentValue);
+            if (exists)
+            {
+                masterMixer.SetFloat("SFXVolume", Mathf.Lerp(-80, 20, volume));
+            }
+            else
+            {
+                Debug.LogError("❌ SFXVolume n'existe pas dans l'AudioMixer !");
+            }
+        }
+        else
+        {
+            Debug.LogError("❌ AudioMixer non assigné dans l'Inspector !");
+        }
     }
 }
